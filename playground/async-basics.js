3d94a510617802d4ff450aa7cfe2eb5ada0c763e
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-19 16:31:38 +0900

    second settimeout

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -4,4 +4,15 @@ setTimeout(() => {
   console.log('inside of callback');
 }, 2000);
 
+setTimeout(() => {
+  console.log('second setTimeout');
+}, 0);
+
 console.log('finishing app');
+
+
+//order of logs:
+//1 - starting app
+//2 - finishing app
+//3 - second setTeimeout
+//4 - inside of callback
