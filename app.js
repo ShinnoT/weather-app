5a321f0444dafd74bf5e7eba6fafbee3d8b78978
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-21 17:32:38 +0900

    address, long and lat logged. require yargs but not yet installed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // request module
 const request = require('request');
+const yargs = require('yargs');
 
-
-
+const argv = yargs.argv
 // https request to google maps API to fetch location and coordinates
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=tokyo%20impact%20hub',
@@ -13,6 +13,12 @@ request({
   console.log(body);
   // log the object prettier
   // console.log(JSON.stringify(body, undefined, 2));
+
+  // logging address and long lat
+  console.log(`Adress: ${body.results[0].formatted_address}`);
+  let lat = body.results[0].geometry.location.lat;
+  let long = body.results[0].geometry.location.lng;
+  console.log(`Latitude: ${lat}, Longitude: ${long}`);
 });
 
 // error will reveal the local error (mistyped url, own computer not connected to internet, etc)
