e6883284a709beb8a0f6da35db495abbcc0f6c26
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-21 19:58:48 +0900

    request logic moved to geocode.js

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,3 +1,38 @@
 let geocodeAddress = (address) => {
+  let encodedAddress = encodeURIComponent(address);
+  // let encodedAddress = encodeURIComponent(argv.address);
+  // there is also decodeURIComponent('blah blah');
 
+  // https request to google maps API to fetch location and coordinates
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+    console.log(error);
+    // error will reveal the local error (mistyped url, own computer not connected to internet, etc)
+    // body.statusCode will reveal the server error (404, 500, etc. which are problems with google for example and not local)
+    console.log(response);
+    console.log(body);
+    // log the object prettier
+    // console.log(JSON.stringify(body, undefined, 2));
+
+
+    if (error) {
+      console.log('unable to access google servers, sorry.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('unable to find that address');
+    } else if (body.status === 'OK') {
+      // logging address and long lat
+      console.log(`Adress: ${body.results[0].formatted_address}`);
+      let lat = body.results[0].geometry.location.lat;
+      let long = body.results[0].geometry.location.lng;
+      console.log(`Latitude: ${lat}, Longitude: ${long}`);
+    }
+
+  });
 };
+
+
+module.exports = {
+  encodedAddress
+}
