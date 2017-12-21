332a7c4fcb58f6d964b0adcd13bd709b88706a85
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-21 20:17:32 +0900

    notes

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ geocode.geocodeAddress(argv.address, (errorMessage, correctResult) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(correctResult));
+    console.log(JSON.stringify(correctResult, undefined, 2));
+    // the first is stringifying, undefined is unneccessary parameter, 2 is spacing for object
+    // the undefined here is unrelated to the callback parameter in geocode line 36
   }
 });
 // geocode.geocodeAddress(argv.a); ?????? does this work?
