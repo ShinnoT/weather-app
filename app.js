b8f8d702ded23ad1dc90ef02de31f4e52f3ee5c5
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-21 16:53:45 +0900

    https request from google maps API

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // request module
-
+const request = require('request');
 
 
 
 // https request to google maps API to fetch location and coordinates
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=tokyo%20impact%20hub',
+  json: true
+}, (error, response, body) => {
+  console.log(error);
+  console.log(response);
+  console.log(body);
+});
+
