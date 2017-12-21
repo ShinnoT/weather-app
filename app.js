9fbb0e4362138281c6c382f07aac32f3695eb894
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-21 17:11:03 +0900

    notes

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ request({
   console.log(error);
   console.log(response);
   console.log(body);
+  // log the object prettier
+  // console.log(JSON.stringify(body, undefined, 2));
 });
 
+// error will reveal the local error (mistyped url, own computer not connected to internet, etc)
+// body.statusCode will reveal the server error (404, 500, etc. which are problems with google for example and not local)
