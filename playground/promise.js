5e391389bc49ef5366494eb967000b034bf57fa5
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-23 21:56:57 +0900

    promise example and notes

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,20 @@
 //JS promise practice
 //what are they and how do they work?
 
+let somePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('able to fulfill promise');
+  }, 2500);
+});
+
+//you can either resolve or reject ONLY once
+//you cant do both
+//you cant do more than once
+
+somePromise.then((message) => {
+  console.log('success: ', message);
+  // will print if promise was resolved (fullfilled)
+}, (errorMessage) => {
+  console.log('error: ', errorMessage);
+  // will print if promise was rejected (NOT fullfilled)
+});
